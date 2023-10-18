9a90b0fde2b199d91f2f10f4d48b24647f9e0edd
Sergi <sergikusakin@gmail.com>
2023-10-18 12:51:20 +0200

    create PostIdPage

---
+++ src/pages/PostIdPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is page with ID Nº{{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
