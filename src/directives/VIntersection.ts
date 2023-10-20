d0e2b039b80932d1f3736c9c199270a744d9f7ba
Sergi <sergikusakin@gmail.com>
2023-10-20 14:57:44 +0200

    change of VIntersection

--- src/directives/VIntersection.ts
+++ src/directives/VIntersection.ts
@@ -5,11 +5,13 @@ export default {
       threshold: 1.0,
     };
     const callback = (entries: any, observer: any) => {
-      if (entries[0].isIntersecting && ((this.page < this.totalPages) as any)) {
+      if (entries[0].isIntersecting) {
+        // if (entries[0].isIntersecting && ((this.page < this.totalPages) as any)) {
         binding.value();
       }
     };
     const observer = new IntersectionObserver(callback, options);
     observer.observe(el);
   },
+  name: "Intersection",
 };
